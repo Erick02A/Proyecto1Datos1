bd8a130e57be3395a6a11c2c3aed9e297949138f
Erick02A <102835005+Erick02A@users.noreply.github.com>
2022-08-29 21:56:06 -0600

    .
    .

--- src/main/java/com/example/proyecto1datos1/MusicPro.java
+++ src/main/java/com/example/proyecto1datos1/MusicPro.java
@@ -12,7 +12,7 @@ public class MusicPro extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MusicPro.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Probando commit");//Hola como estas?
+        stage.setTitle("Probando commit");
         stage.setScene(scene);
         stage.show();
     }
