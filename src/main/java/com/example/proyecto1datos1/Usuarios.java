f5d8a1ca11140c3f52057a225a236e9e45b6bef3
Erick02A <102835005+Erick02A@users.noreply.github.com>
2022-09-04 20:55:19 -0600

    Usuarios
    Se agrega la clase del objeto usuario.

---
+++ src/main/java/com/example/proyecto1datos1/Usuarios.java
@@ -0,0 +1,15 @@
+package com.example.proyecto1datos1;
+
+public class Usuarios {
+    private String nombre;
+    private String correo;
+    private String provincia;
+    private String contraseña;
+
+    public void usuarios() {
+        this.nombre = nombre;
+        this.correo = correo;
+        this.provincia = provincia;
+        this.contraseña = contraseña;
+    }
+}
