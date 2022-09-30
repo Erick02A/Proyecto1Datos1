33f6c37f1ec0e593f55df63bce072df347fa78a5
Erick02A <102835005+Erick02A@users.noreply.github.com>
2022-09-29 22:47:05 -0600

    Se arregla el verificar cada lista por ususario

--- src/main/java/com/example/proyecto1datos1/listaSongs.java
+++ src/main/java/com/example/proyecto1datos1/listaSongs.java
@@ -2,18 +2,35 @@ package com.example.proyecto1datos1;
 
 import java.io.File;
 
+/**
+ * listaSongs se encarga de crear listas doblemente enlasadas con objetos tipo Song.
+ */
 public class listaSongs {
     private Songs cabeza;
     private Songs last;
     private int size;
     private boolean bucle;
 
+    /**
+     * Crea la lista inicial con cabeza y last nulos, zice en 0  y si es circular en false.
+     */
     public listaSongs(){
         this.cabeza = null;
         this.last = null;
         this.size = 0;
         this.bucle = false;
     }
+
+    /**
+     * Añade un objeto de tipo cancion al principio de la lista, resiviendo como parametros los atributos que va a tener el objeto Song.
+     * @param cancion
+     * @param genero
+     * @param artista
+     * @param album
+     * @param año
+     * @param letra
+     * @param data
+     */
     public void addsongfirst(String cancion, String genero, String artista, String album, String año, String letra, File data){
         if (cabeza==null){
             cabeza = new Songs(cancion, genero, artista, album, año, letra, data);
@@ -27,6 +44,17 @@ public class listaSongs {
         }
         size++;
     }
+
+    /**
+     * Añade un objeto de tipo cancion al final de la lista, resiviendo como parametros los atributos que va a tener el objeto Song.
+     * @param cancion
+     * @param genero
+     * @param artista
+     * @param album
+     * @param año
+     * @param letra
+     * @param data
+     */
     public void addsonglast(String cancion, String genero, String artista, String album, String año, String letra, File data){
         if (cabeza==null){
             cabeza = new Songs(cancion, genero, artista, album, año, letra, data);
@@ -39,6 +67,11 @@ public class listaSongs {
         }
         size++;
     }
+
+    /**
+     * Esta elimina un objeto tipo Song de la lista enlasada de Songs.
+     * @param song Objeto tipo Song que se busca eliminar.
+     */
     public void eliminar(Songs song){
         if (bucle==true){
             if (song == cabeza){
@@ -62,12 +95,17 @@ public class listaSongs {
             }
         }
     }
-    public int getSize(){
-        return size;
-    }
+    /**
+     * Esta pregunta si la lista se encuentra vacía.
+     * @return boolean que dice si es vacia.
+     */
     public boolean estaVacio(){
         return (cabeza==null)?true:false;
     }
+    /**
+     * Esta hace que la lista se vuelva circular o hace que deje de serlo.
+     * @param a boolean que dice si la lista ya era circular.
+     */
     public void bucle(Boolean a){
         if(a==false) {
             last.setNext(cabeza);
@@ -79,15 +117,11 @@ public class listaSongs {
             bucle = false;
         }
     }
-    public String obtener(int index){
-        int contador = 0;
-        Songs temporal = cabeza;
-        while (contador<index){
-            temporal=temporal.getNext();
-            contador++;
-        }
-        return temporal.getCancion();
-    }
+
+    /**
+     * Esta retorna la cabeza de la lista
+     * @return Objeto tipo Song, cabeza de la lista.
+     */
     public Songs getCabeza(){
         return cabeza;
     }
