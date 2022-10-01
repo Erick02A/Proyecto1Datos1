ee4c6b23dde82efbb5a00085a3351eb62d94774f
Erick02A <102835005+Erick02A@users.noreply.github.com>
2022-09-30 19:22:11 -0600

    incorporando javadoc

--- src/main/java/com/example/proyecto1datos1/Songs.java
+++ src/main/java/com/example/proyecto1datos1/Songs.java
@@ -3,7 +3,7 @@ package com.example.proyecto1datos1;
 import java.io.*;
 
 /**
- * Nodo de canciones en las que se instancia cada una
+ * Nodo de canciones en las que se instancia cada una.
  */
 public class Songs {
     private String cancion;
@@ -12,9 +12,9 @@ public class Songs {
     private Songs prev;
 
     /**
-     *
-     * @param cancion
-     * @param data
+     * Inicializa lel nodo con sus variables.
+     * @param cancion Nobre de la cancion que se le ingresa al crearlo.
+     * @param data Objeto tipo File que trae la cancion de archivos.
      */
     public Songs(String cancion, File data) {
         this.cancion = cancion;
@@ -24,48 +24,48 @@ public class Songs {
     }
 
     /**
-     *
-     * @return
+     * Retorna el nodo anterior a este.
+     * @return Nodo Songs
      */
     public Songs getPrev() {
         return prev;
     }
 
     /**
-     *
-     * @param prev
+     * Cambia cual nodo va entes de este.
+     * @param prev Nodo Songs
      */
     public void setPrev(Songs prev) {
         this.prev = prev;
     }
 
     /**
-     *
-     * @return
+     * Retorna el archivo media de la cancion.
+     * @return File data media.
      */
     public File getdata() {
         return data;
     }
 
     /**
-     *
-     * @return
+     * Retorna el nombrfe de la cancion del nodo.
+     * @return String nombre de la cancion.
      */
     public String getCancion() {
         return cancion;
     }
 
     /**
-     *
-     * @param songs
+     * Cambia el nodo que va despues de este
+     * @param songs Nodo Songs
      */
     public void setNext(Songs songs){
         next = songs;
     }
 
     /**
-     *
-     * @return
+     * Retorna el nodo que va despues de este.
+     * @return Nodo Songs
      */
     public Songs getNext(){
         return next;
